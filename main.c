2122590f5bf238f3f43e269982fbf23ea11cd75b
MochTor <tieghi.marco@gmail.com>
2016-07-14 01:12:48 +0200

    Created function to generate frequency vector
    This function creates the vector containing the first 256 color
    frequencies of image palette

--- main.c
+++ main.c
@@ -12,8 +12,10 @@ typedef struct {
 //
 
 void compressImage(PPMImage *, myImage *, int);
-//void findFrequency();
+//struct color* findFrequency(struct color*, int);
 int colorList(PPMImage *);
+int maxFrequency(struct color*);
+int* frequencyVector(struct color *el);
 
 int main(int argc, char const *argv[]) {
     PPMImage *image;
@@ -38,6 +40,7 @@ void compressImage(PPMImage *image, myImage *resultImage, int colorNumber) {
     int paletteSize;
     struct color *colorPointer = returnHead();  //Initialized to the head of the palette
     int i = 0;
+    int *frequencies = NULL;
 
     //Allocating resulting image data
     resultImage = (myImage *) malloc(sizeof(myImage));
@@ -51,21 +54,32 @@ void compressImage(PPMImage *image, myImage *resultImage, int colorNumber) {
     else
         paletteSize = colorNumber;
 
-    //For the moment, I've just put the first 256 colors of the palette, without filtering them
-    while (i < paletteSize) {
-        resultImage->colors[i].red = colorPointer->red;
-        resultImage->colors[i].green = colorPointer->green;
-        resultImage->colors[i].blue = colorPointer->blue;
-        printf("Filling palette with color number %d; values: %d, %d, %d\n", i+1, resultImage->colors[i].red, resultImage->colors[i].green, resultImage->colors[i].blue);
-
-        colorPointer = colorPointer->next;
-
-        i++;
+    frequencies = frequencyVector(colorPointer);
+    int ii;
+    for (ii = 0; ii < 256; ii++) {
+        printf("Frequency N %d: %d\n", ii, frequencies[ii]);
     }
+    // int max = maxFrequency(colorPointer);
+    // printf("Max frequency: %d\n", max);
+    //struct color *maxColor = findFrequency(colorPointer, max);
+    //printf("Color with maximum frequency: %d, %d, %d\n", maxColor->red, maxColor->green, maxColor->blue);
+
+    //For the moment, I've just put the first 256 colors of the palette, without filtering them
+    // while (i < paletteSize) {
+    //     resultImage->colors[i].red = colorPointer->red;
+    //     resultImage->colors[i].green = colorPointer->green;
+    //     resultImage->colors[i].blue = colorPointer->blue;
+    //     //printf("Filling palette with color number %d; values: %d, %d, %d, with frequency %d \n", i+1, resultImage->colors[i].red, resultImage->colors[i].green, resultImage->colors[i].blue, colorPointer->frequency);
+    //
+    //     colorPointer = colorPointer->next;
+    //
+    //     i++;
+    // }
+
 }
 
 /**
- * This function finds the number of unique colors of a PPM image
+ * This function finds the number of unique colors of a PPM image and fills the palette
  * @param  image: the image for which to find the color number
  * @return       the number of colors
  */
@@ -82,4 +96,50 @@ int colorList(PPMImage *image){
         }
     }
     return colorNumber;
+}
+
+int maxFrequency(struct color* el) {
+    int max = 0;
+    while (el) {
+        if (el->frequency > max) {
+            max = el->frequency;
+        }
+        el = el->next;
+    }
+    return max;
+}
+
+// struct color* findFrequency(struct color* el, int requiredFrequency) {
+//     while (el->frequency != requiredFrequency) {
+//         el = el->next;
+//     }
+//     return el;
+// }
+
+/**
+ * This function bulds the frequency vector for colors in the color list:
+ * it inserts frequencies in decreasing order [it consider just the first 256 frequencies,
+ * which may not correspond to the first 256 colors]
+ * @param  head: head of the list from which create frequencies vector
+ * @return      pointer to the vector
+ */
+int* frequencyVector(struct color *head) {
+    struct color* el = head;
+    int max = maxFrequency(el);
+    int secondMax;
+    int* frequencyArray = (int *) malloc(sizeof(int)*256);
+    frequencyArray[0] = max;
+    for (int i = 1; i < 256; i++) {
+        secondMax = 0;
+        while (el) {
+            if (el->frequency < max && el->frequency > secondMax) {
+                secondMax = el->frequency;
+            }
+            el = el->next;
+        }
+        frequencyArray[i] = secondMax;
+        max = secondMax;
+        el = head;
+    }
+    return frequencyArray;
 }
